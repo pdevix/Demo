c166d72ef8eaea8b236a10b908d0b5296c31d1c1
pdevix <priyankha@multicorewareinc.com>
2024-01-08 21:54:37 +0530

    Inference modified

--- TF_Inference_cifar.py
+++ TF_Inference_cifar.py
@@ -1,4 +1,4 @@
-import tensorflow-cpu as tf
+import tensorflow as tf
 from tensorflow.keras import datasets, layers, models
 
 #Download and Prepare Cifar dataset
@@ -27,7 +27,7 @@ model.compile(optimizer='adam',
               metrics=['accuracy'])
 
 #Training the model
-history = model.fit(train_images, train_labels, epochs=10, 
+history = model.fit(train_images, train_labels, epochs=2, 
                     validation_data=(test_images, test_labels))
 
 #Evaluate
